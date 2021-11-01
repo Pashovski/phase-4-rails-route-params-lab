8507a759ae214e7e8eec57da6d871e5bd8e0e020
Pasha <pashdoctor@gmail.com>
2021-11-01 11:17:24 -0400

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,18 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+  byebug
+    if params[:name]
+     students = students.select {|student| student.first_name == params[:name] || student.last_name == params[:name]}
+    end
+
     render json: students
   end
 
+
+  def show
+    student = Student.find(params[:id])
+
+    render json: student
+  end
 end
